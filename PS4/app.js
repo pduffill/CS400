6098b0e2ef7b8bff463ecff5fb4e6f18a41da13a
pduffill <43013394+pduffill@users.noreply.github.com>
2020-04-01 16:58:44 -0400

    Finished ps4 after postman gave me the wrong options for my GET request

--- PS4/app.js
+++ PS4/app.js
@@ -22,7 +22,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/pS4', ps4Router);
+app.use('/ps4', ps4Router);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- PS4/routes/ps4.js
+++ PS4/routes/ps4.js
@@ -1,27 +1,29 @@
 let express = require('express');
 let router = express.Router();
 
-var request = require('request');
-var options = {
-  'method': 'GET',
-  'url': 'boredapi.com/api/activity?type=recreational',
-  'headers': {
-  }
+const https = require('https');
+const options = {
+  hostname: 'www.boredapi.com',
+  path: '/api/activity',
+  method: 'GET',
+  type: 'recreational'
 };
 
-request(options, function (error, response) { 
-  if (error) throw new Error(error);
-  console.log(response.body);
-});
 
 router.get('/', function(req, res, next){
-    let req1 = https.request(options, resp => {
+    const req1 = https.request(options, resp => {
         console.log(`statusCode: ${resp.statusCode}`)
 
         resp.on('data', d => {
             res.render('ps4', {title: 'Here\'s a recreational activity to do!', activity: JSON.parse(d.toString()).activity, price: JSON.parse(d.toString()).price})
         })
     })
+
+    req1.on('error', error => {
+      console.error(error)
+  })
+  
+  req1.end()
 });
 
-module.exports(router);
+module.exports = router;
